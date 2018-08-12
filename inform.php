232e17856fde4a1858621dd4d934cb7c10442ee8
David <zuccaro.david@gmail.com>
2018-08-12 19:17:22 +1000

    Added timezones

--- inform.php
+++ inform.php
@@ -1,9 +1,10 @@
 <?php
 
-# David Zuccaro 09/08/2018                       
-# Miscellaneous Information                                        
-# World Clock 
-# 11/08/2018 Added crypto currency prices                                                     
+// David Zuccaro 09/08/2018                       
+// Miscellaneous Information                                        
+// World Clock 
+// 11/08/2018 Added crypto currency prices                                                     
+// 12/08/2018 Added FOREX 
 
 ?>
 <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
@@ -21,10 +22,10 @@
     <div id="wrapper">
         <?php 
             function CallAPI($method, $url, $data = false, $key = false)
-            # method: html request method
-            # url: the web address
-            # data: dat to append to url
-            # key: the access key to the web site
+            // method: html request method
+            // url: the web address
+            // data: dat to append to url
+            // key: the access key to the web site
             {
                 $curl = curl_init();
 
@@ -45,11 +46,10 @@
                         if ($data)
                         {
                             $info = http_build_query($data);
-                            // echo($info);
                             $url = sprintf('%s?%s', $url, $info);
                         }
                 }
-                // echo($url);
+
                 if ($key)
                     curl_setopt($curl, CURLOPT_HTTPHEADER, array('content-type: application/x-www-form-urlencoded', 'x-api-key: 352b69e93c5a43d513e4db1e4803019f352b69e9'));
                 
@@ -66,82 +66,15 @@
                 curl_close($curl);
                 if ($err) 
                 {
-                    echo "curl Error #:" . $err;
+                    echo "curl Error //:" . $err;
                 }               
 
                 return $result;
-            }
+            }           
             
+            // FOREX
             setlocale(LC_MONETARY, "en_US");
-            $FIXER_API_KEY = "b9c6a28b6be2e9bd243c14a77766ea32";
-            
-            echo(PHP_EOL);
-            echo('      <div id="outer1">' . PHP_EOL);
-            echo('          <div class="box1">'. PHP_EOL);
-            echo('              <h2>' . PHP_EOL);
-            echo('                  World Clock' . PHP_EOL);
-            echo('              </h2>' . PHP_EOL);                 
-                 
-            $json = CallAPI('GET', 'https://api.coindesk.com/v1/bpi/currentprice/AUD.json', false, false);
-            $data = json_decode($json, TRUE);
-            $time = date("H:i");                        
-            echo('              <span class="time"><span class="home">Melbourne</span></span>' . PHP_EOL);
-            echo('              <span class="timed"><span class="home">' . $time . '</span></span>' . PHP_EOL);                        
-            $time2 = new DateTime($time);
-            
-            echo('              <span class="time">Rome</span>' . PHP_EOL);   
-            $time2 = $time2->setTimeZone(new DateTimeZone("Europe/Rome"));
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
-                                    
-            echo('              <span class="time">London</span>' . PHP_EOL);                
-            $time2 = $time2->setTimeZone(new DateTimeZone("Europe/London"));           
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
-            
-            echo('              <span class="time">Moscow</span>' . PHP_EOL);                
-            $time2 = $time2->setTimeZone(new DateTimeZone("Europe/Moscow"));           
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
-            
-            echo('              <span class="time">Los Angeles</span>' . PHP_EOL);        
-            $time2 = $time2->setTimeZone(new DateTimeZone("America/Los_Angeles"));   
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
-            
-            echo('              <span class="time">New York</span>' . PHP_EOL);        
-            $time2 = $time2->setTimeZone(new DateTimeZone("America/New_York"));   
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
-            
-            echo('              <span class="time">Shanghai</span>' . PHP_EOL);        
-            $time2 = $time2->setTimeZone(new DateTimeZone("Asia/Shanghai"));   
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
-            
-            echo('              <span class="time">Jakata</span>' . PHP_EOL);        
-            $time2 = $time2->setTimeZone(new DateTimeZone("Asia/Jakarta"));   
-            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);
-            
-            echo('          </div>' . PHP_EOL);
-            echo('      </div><!-- end #outer1 -->' . PHP_EOL);
-            echo('      <div id="outer2">' . PHP_EOL);
-            echo('          <div class="box1">' . PHP_EOL);
-            echo('          <h2>' . PHP_EOL);
-            echo('              Cryptocurrency Prices' . PHP_EOL);
-            echo('          </h2>' . PHP_EOL);
-            echo('          <span class="time">1 BTC = $AUD ' . $data["bpi"]["AUD"]["rate"] . '</span>');
-            echo('          </div>' . PHP_EOL);
-            echo('      </div><!-- end #outer2 -->' . PHP_EOL);
-            echo('      <div id="outer3">' . PHP_EOL);
-            echo('          <div class="box1">' . PHP_EOL);
-            echo('          <h2>' . PHP_EOL);
-            echo('              Commodities' . PHP_EOL);
-            echo('          </h2>' . PHP_EOL);
-            $gold_json = CallAPI('GET', 'http://goldpricez.com/api/rates/currency/usd/measure/all', false, '352b69e93c5a43d513e4db1e4803019f352b69e9');
-            $gd = str_replace("\\", "", $gold_json);        
-            $gd = substr($gd,1,-1);
-            $gold_data = json_decode($gd, TRUE);           
-            echo('              <span class="time">Gold: $' . $gold_data["ounce_price_usd"] . ' per ounce' . '</span>' . PHP_EOL);   
-            echo('          </h2>' . PHP_EOL);
-            echo('          </div>' . PHP_EOL);
-            echo('      </div><!-- end #outer3 -->' . PHP_EOL);
-            echo('      <div id="outer4">' . PHP_EOL);
-            echo('          <div class="box1">' . PHP_EOL);
+            $FIXER_API_KEY = "b9c6a28b6be2e9bd243c14a77766ea32"; //forex
             $params = new \stdClass();
             $params->access_key = $FIXER_API_KEY;
             $params->base = "EUR";
@@ -151,6 +84,99 @@
             $usdaud = $data["rates"]["USD"] / $data["rates"]["AUD"];
             $gbpaud = $data["rates"]["GBP"] / $data["rates"]["AUD"];
             $euraud = 1.0 / $data["rates"]["AUD"];
+            
+            // Crypto
+            $json = CallAPI('GET', 'https://api.coindesk.com/v1/bpi/currentprice/AUD.json', false, false);
+            $data_btc = json_decode($json, TRUE);
+            $btc_price_aud = str_replace(",", "", $data_btc["bpi"]["AUD"]["rate"]);                       
+            
+            // Commodities
+            $gold_json = CallAPI('GET', 'http://goldpricez.com/api/rates/currency/usd/measure/all', false, '352b69e93c5a43d513e4db1e4803019f352b69e9');
+            $gd = str_replace("\\", "", $gold_json);        
+            $gd = substr($gd,1,-1);
+            $gold_data = json_decode($gd, TRUE);             
+            $gold_aud = $gold_data["ounce_price_usd"] / $usdaud;
+            
+            echo(PHP_EOL);
+            echo('      <div id="outer1">' . PHP_EOL);
+            echo('          <div class="box1">'. PHP_EOL);
+            echo('              <h2>' . PHP_EOL);
+            
+            // Timezones
+            echo('                  World Clock' . PHP_EOL);
+            echo('              </h2>' . PHP_EOL);                                  
+            
+            $time = date("H:i");                  
+            echo('              <span class="time"><span class="home">Melbourne</span></span>' . PHP_EOL);
+            echo('              <span class="timed"><span class="home">' . $time . '</span></span>' . PHP_EOL);                        
+            $time2 = new DateTime($time);
+            
+            echo('              <span class="time">Jakata</span>' . PHP_EOL);        
+            $time2 = $time2->setTimeZone(new DateTimeZone("Asia/Jakarta"));   
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);
+            
+            echo('              <span class="time">Johannesburg</span>' . PHP_EOL);        
+            $time2 = $time2->setTimeZone(new DateTimeZone("Africa/Johannesburg"));   
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);                        
+            
+            echo('              <span class="time">London</span>' . PHP_EOL);                
+            $time2 = $time2->setTimeZone(new DateTimeZone("Europe/London"));           
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);              
+            
+            echo('              <span class="time">Los Angeles</span>' . PHP_EOL);        
+            $time2 = $time2->setTimeZone(new DateTimeZone("America/Los_Angeles"));   
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);              
+            
+            echo('              <span class="time">Moscow</span>' . PHP_EOL);                
+            $time2 = $time2->setTimeZone(new DateTimeZone("Europe/Moscow"));           
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
+            
+            echo('              <span class="time">New Dehli</span>' . PHP_EOL);                
+            $time2 = $time2->setTimeZone(new DateTimeZone("Asia/Kolkata"));           
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);  
+            
+            echo('              <span class="time">New York</span>' . PHP_EOL);        
+            $time2 = $time2->setTimeZone(new DateTimeZone("America/New_York"));   
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);              
+            
+            echo('              <span class="time">Rome</span>' . PHP_EOL);   
+            $time2 = $time2->setTimeZone(new DateTimeZone("Europe/Rome"));
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);                                                                                 
+            
+            echo('              <span class="time">Shanghai</span>' . PHP_EOL);        
+            $time2 = $time2->setTimeZone(new DateTimeZone("Asia/Shanghai"));   
+            echo('              <span class="timed">' . $time2->format("H:i") . '</span>' . PHP_EOL);              
+            
+            echo('          </div>' . PHP_EOL);
+            echo('      </div><!-- end //outer1 -->' . PHP_EOL);
+            
+            // Crypto
+            echo('      <div id="outer2">' . PHP_EOL);
+            echo('          <div class="box1">' . PHP_EOL);
+            echo('              <h2>' . PHP_EOL);
+            echo('                  Cryptocurrency Prices' . PHP_EOL);
+            echo('              </h2>' . PHP_EOL);
+            // echo(PHP_EOL);
+            // echo($btc_price_aud);
+            //echo(PHP_EOL);
+            echo('              <span class="time">1 BTC = $AUD ' . money_format('%9.2i', $btc_price_aud) . '</span>');
+            echo('          </div><!-- end box1 -->' . PHP_EOL); 
+            echo('      </div><!-- end //outer2 -->' . PHP_EOL);
+            
+            // Commodities
+            echo('      <div id="outer3">' . PHP_EOL);
+            echo('          <div class="box1">' . PHP_EOL);
+            echo('          <h2>' . PHP_EOL);
+            echo('              Commodities' . PHP_EOL);
+            echo('          </h2>' . PHP_EOL);           
+            echo('              <span class="time">Gold: $' . money_format('%7.2i', $gold_aud) . ' per ounce' . '</span>' . PHP_EOL);   
+            echo('          </h2>' . PHP_EOL);
+            echo('          </div>' . PHP_EOL);
+            echo('      </div><!-- end //outer3 -->' . PHP_EOL);
+            
+            // Forex
+            echo('      <div id="outer4">' . PHP_EOL);
+            echo('          <div class="box1">' . PHP_EOL);            
             echo('          <h2>' . PHP_EOL);
             echo('              FOREX (AUD)' . PHP_EOL);
             echo('          </h2>' . PHP_EOL);
@@ -158,7 +184,7 @@
             echo('              <span class="time">EUR: ' . money_format('%2.5i', $euraud) . '</span>' . PHP_EOL);   
             echo('              <span class="time">GBP: ' . money_format('%2.5i', $gbpaud) . '</span>' . PHP_EOL);   
             echo('          </div>' . PHP_EOL);
-            echo('      </div><!-- end #outer4 -->' . PHP_EOL);
+            echo('      </div><!-- end //outer4 -->' . PHP_EOL);
             echo('      <div id="footer">' . PHP_EOL);
             echo('      <div class="box2">' . PHP_EOL);
             echo('          <span class="footer">' . PHP_EOL);
@@ -167,8 +193,6 @@
             echo('          </span>' . PHP_EOL);
             echo('      </div>' . PHP_EOL);
             echo('  </div>' . PHP_EOL);
-            //var_dump(json_decode($json, true));
-            //echo count($data);
         ?>
-    </div> <!-- end #wrapper -->
+    </div> <!-- end //wrapper -->
 </body>
